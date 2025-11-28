f42fc0214b23725332b85e4b79c8b19c7712083f
Merlin Elvers <elvers@uni-bremen.de>
2025-11-28 16:04:10 +0100

    Add string argument type

--- src/fortuno/argumentparser.f90
+++ src/fortuno/argumentparser.f90
@@ -71,14 +71,15 @@ module fortuno_argumentparser
   end interface
 
 
-  !> Collection of all argument values obtained after command line had been prased
+  !> Collection of all argument values obtained after command line had been parsed
   type :: argument_values
     private
     type(argument_value), allocatable :: argvals(:)
   contains
     procedure :: has => argument_values_has
     procedure :: get_value_stringlist => argument_values_get_value_stringlist
-    generic :: get_value => get_value_stringlist
+    procedure :: get_value_string => argument_values_get_value_string
+    generic :: get_value => get_value_stringlist, get_value_string
   end type argument_values
 
 
@@ -204,7 +205,31 @@ contains
                   call move_alloc(argvalbuffer, argumentvalues%argvals)
                 end block
                 ! +}
-
+              case (argtypes%string)
+                ! Require next argument as value
+                iarg = iarg + 1
+                if (iarg > nargs) then
+                  call logger%log_error("Missing value for argument '" // arg // "'")
+                  exitcode = 1
+                  return
+                end if
+                ! Workaround:gfortran:14.1 (bug 116679)
+                ! Omit array expression to avoid memory leak
+                ! {-
+                ! argumentvalues%argvals = [argumentvalues%argvals,&
+                !     & argument_value(argdef%name, argval=string_item(cmdargs(iarg)%value))]
+                ! -}{+
+                block
+                  type(argument_value), allocatable :: argvalbuffer(:)
+                  integer :: nn
+                  nn = size(argumentvalues%argvals)
+                  allocate(argvalbuffer(nn + 1))
+                  argvalbuffer(1 : nn) = argumentvalues%argvals
+                  argvalbuffer(nn + 1) = argument_value(argdef%name, &
+                      & argval=string_item(cmdargs(iarg)%value))
+                  call move_alloc(argvalbuffer, argumentvalues%argvals)
+                end block
+                ! +}
               case default
                 call logger%log_error("Unknown argument type")
                 exitcode = 1
@@ -318,6 +343,45 @@ contains
     end if
 
   end subroutine argument_values_get_value_stringlist
+
+
+  !> Returns the value of a parsed argument as a single string
+  subroutine argument_values_get_value_string(this, name, val)
+
+    !> Instance
+    class(argument_values), intent(in) :: this
+
+    !> Name of the argument
+    character(*), intent(in) :: name
+
+    !> Value on exit
+    type(string_item), allocatable, intent(out) :: val
+
+    logical :: found
+    integer :: iargval
+    
+    ! Find argument position
+    found = .false.
+    do iargval = 1, size(this%argvals)
+      found = this%argvals(iargval)%name == name
+      if (found) exit
+    end do
+
+    if (.not. found) then
+      error stop "Argument '" // name // "' not found"
+    end if
+    
+    select type (argval => this%argvals(iargval)%argval)
+    type is (string_item)
+      val = argval
+    class default
+      error stop "Invalid argument type for argument '" // name // "' (expected string_item)"
+    end select
+
+  end subroutine argument_values_get_value_string
+
+
+
 
 
   !> User defined structure constructor for argument_value
